0e239042e5c4f65b6a3108e9a079339a02cc97d1
AanviKothari456 <kothariaanvi@gmail.com>
2025-06-03 19:49:42 +0530

    Serve React frontend + include build

--- app.py
+++ app.py
@@ -63,9 +63,12 @@ def teacher_comments():
 from flask import send_from_directory
 import os
 
+from flask import send_from_directory
+import os
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def serve_react(path):
+def serve(path):
     if path != "" and os.path.exists("client/build/" + path):
         return send_from_directory('client/build', path)
     else:
